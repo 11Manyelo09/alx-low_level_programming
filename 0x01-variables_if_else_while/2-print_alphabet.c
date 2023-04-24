d642944387301e7b429f28dad124c5e604ba5f92
11Manyelo09 <manyelomapula.com>
2023-04-24 13:50:39 -0700

    2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -2,20 +2,16 @@
 
 /**
  * main - prints the alphabet in lowercase,
- * followed by a new line, except q and e
- * Return: Aways 0 (Sucess)
+ * followed by a new line
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	char ch = 'a';
+	char ch;
 
-	while (ch <= 'z')
+	for (ch = 'a'; ch <= 'z'; ch++)
 	{
-		if (ch != 'e' && ch != 'q')
-		{
-			putchar(ch);
-		}
-		ch++;
+		putchar(ch);
 	}
 	putchar('\n');
 	return (0);
