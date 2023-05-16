cb46fd0ad8a6ee4d5c31bdeb872a569980207025
11Manyelo09 <manyelomapula.com>
2023-05-16 12:58:28 -0700

    0x0B. C - malloc, free

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -9,7 +9,7 @@
 char *_strdup(char *str)
 {
 	char *aaa;
-	
+
 	int i, r = 0;
 
 	if (str == NULL)
@@ -23,7 +23,7 @@ char *_strdup(char *str)
 
 	if (aaa == NULL)
 		return (NULL);
-	
+
 	for (r = 0; str[r]; r++)
 
 		aaa[r] = str[r];
